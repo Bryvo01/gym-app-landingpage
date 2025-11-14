3dfb8bb7a1d76fd8dcdb2805e443de8a59288c24
Bryan LeVau <bryvo01@garflak.com>
2025-11-14 05:44:09 -0600

    adds plan distinctions

--- src/components/Pricing.svelte
+++ src/components/Pricing.svelte
@@ -22,21 +22,26 @@
           <h3 class="text-xl font-semibold text-stone-900">Basic</h3>
           <p class="mt-2 text-3xl font-bold text-stone-900">$50<span class="text-lg font-medium text-stone-600">/mo</span></p>
           <p class="mt-1 text-base text-stone-600">($500/year)</p>
+          <p class="mt-1 text-base text-stone-600">Unlimited Workouts</p>
+          <p class="mt-1 text-base text-stone-600">Access to all equipment</p>
         </div>
         <div class="rounded-lg bg-white p-6 shadow-lg ring-2 ring-indigo-400">
           <h3 class="text-xl font-semibold text-stone-900">Swoley Training</h3>
           <p class="mt-2 text-3xl font-bold text-stone-900">$75<span class="text-lg font-medium text-stone-600">/mo</span></p>
           <p class="mt-1 text-base text-stone-600">($750/year)</p>
+          <p class="mt-1 text-base text-stone-600">Everything in the Basic Plan</p>
+          <p class="mt-1 text-base text-stone-600">Personalized Training</p>
         </div>
         <div class="rounded-lg bg-white p-6 shadow-lg">
           <h3 class="text-xl font-semibold text-stone-900">Swoley Nutrition</h3>
           <p class="mt-2 text-3xl font-bold text-stone-900">$100<span class="text-lg font-medium text-stone-600">/mo</span></p>
           <p class="mt-1 text-base text-stone-600">($1000/year)</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="location-column">
+          <p class="mt-1 text-base text-stone-600">Everything in the Training Plan</p>
+          <p class="mt-1 text-base text-stone-600">Customized Meal Plans</p>
+          <p class="mt-1 text-base text-stone-600">Nutritional Consultations</p>
+          <p class="mt-1 text-base text-stone-600">Weight Tracking</p>
+          <p class="mt-1 text-base text-stone-600">Meal Prep</p>
+          <p class="location-column">
       <div class="space-y-10">
         <div>
           <h3 class="text-2xl font-semibold text-stone-900">Find Us</h3>
