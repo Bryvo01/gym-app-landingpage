d0267b79f994567923fa6f4b84c9818e9f2fa6f5
Bryan LeVau <bryvo01@garflak.com>
2025-11-14 06:17:07 -0600

    Pricing tweaks

--- src/components/Pricing.svelte
+++ src/components/Pricing.svelte
@@ -18,7 +18,7 @@
         No Contracts!
       </p>
       <div class="mt-6 space-y-6">
-        <div class="py-14 md:py-20 flex flex-col gap-8 bg-[#181b34] text-white text-center items-center justify-center">
+        <div class="py-14 md:py-20 flex flex-col gap-8 bg-[#181b34] text-white">
           <h4 class="text-xl sm:text-2xl md:text-3xl poppins">Basic</h4>
           <p class="poppins text-base sm:text-lg md:text-xl">$50<span class="text-indigo-400">/mo</span></p>
           <p class="poppins text-base sm:text-lg md:text-xl">($500/year)</p>
