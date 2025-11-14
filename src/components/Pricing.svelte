d3cf9eaef31781c5c6ded9bf7a064f5fee7e11e3
Bryan LeVau <bryvo01@garflak.com>
2025-11-14 06:27:32 -0600

    Pricing tweaks

--- src/components/Pricing.svelte
+++ src/components/Pricing.svelte
@@ -19,7 +19,7 @@
       </p>
       <div class="mt-6 space-y-6">
         <div class="py-14 md:py-20 flex flex-col gap-8 bg-[#181b34] text-white">
-          <h4 class="text-xl sm:text-2xl md:text-3xl poppins">Basic<span class="text-left justify-left">$50</span><span class="text-indigo-400">/mo</span></h4>
+          <h4 class="text-xl sm:text-2xl md:text-3xl poppins">Basic<span class="text-right justify-right">$50</span><span class="text-indigo-400">/mo</span></h4>
           <p class="poppins text-base sm:text-lg md:text-xl">($500/year)</p>
           <p class="poppins text-base sm:text-lg md:text-xl">Unlimited Workouts</p>
           <p class="poppins text-base sm:text-lg md:text-xl">Access to all equipment</p>
