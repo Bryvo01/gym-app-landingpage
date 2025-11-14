33d21f581c57a9b7b58721324379b0fd7219ed6e
Bryan LeVau <bryvo01@garflak.com>
2025-11-14 06:35:41 -0600

    removes extra a tag

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex items-center gap-4">
-  <a href="#contact"></a>
+  <a href="#contact">
     <button class={"specialBtn " + (dark ? " bg-[#181b34]" : " ")}>
         <p class="text-base sm:text-lg md:text-xl">Contact Us</p>
     </button>
