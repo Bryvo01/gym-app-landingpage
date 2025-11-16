b73b51321c947ae51b3f71c211e7d3db49231488
Bryan LeVau <bryvo01@garflak.com>
2025-11-16 07:25:31 -0600

    changes +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,26 +17,21 @@
 </script>
 
 {#if $openModal}
-  <div
+<div
     class="fixed top-0 left-0 w-screen h-screen border-b bg-white z-50 flex flex-col gap-8 p-5 px-8 md:hidden"
   >
     <div class="flex items-center justify-between gap-4 border-b pb-2">
-      <h1 class="font-semibold">
-        Swoley <span class="text-indigo-400">Moley</span>
-      </h1>
-      <button
-        on:click={() => ($openModal = false)}
-        class="outline-none border-none"
-      >
-        <i class="fa-solid fa-xmark text-2xl"></i>
-      </button>
-    </div>
+      </div>
     <div class="flex flex-col gap-4 flex-1">
       <button
         on:click={() => reroute("#product")}
         class="border-none outline-none p-2 group duration-200 cursor-pointer text-left"
       >
-        <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">
+        <p
+          class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold border-b-2"
+          class:border-indigo-400={$activeSection === 'product'}
+          class:border-transparent={$activeSection !== 'product'}
+        >
           Product <i class="fa-solid fa-chevron-right text-xl pl-4" />
         </p>
       </button>
@@ -44,7 +39,11 @@
         on:click={() => reroute("#reviews")}
         class="border-none outline-none p-2 group duration-200 cursor-pointer text-left"
       >
-        <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">
+        <p
+          class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold border-b-2"
+          class:border-indigo-400={$activeSection === 'reviews'}
+          class:border-transparent={$activeSection !== 'reviews'}
+        >
           Reviews <i class="fa-solid fa-chevron-right text-xl pl-4" />
         </p>
       </button>
@@ -52,7 +51,11 @@
         on:click={() => reroute("#faqs")}
         class="border-none outline-none p-2 group duration-200 cursor-pointer text-left"
       >
-        <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">
+        <p
+          class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold border-b-2"
+          class:border-indigo-400={$activeSection === 'faqs'}
+          class:border-transparent={$activeSection !== 'faqs'}
+        >
           FAQs <i class="fa-solid fa-chevron-right text-xl pl-4" />
         </p>
       </button>
@@ -60,7 +63,11 @@
         on:click={() => reroute("#pricing")}
         class="border-none outline-none p-2 group duration-200 cursor-pointer text-left"
       >
-        <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">
+        <p
+          class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold border-b-2"
+          class:border-indigo-400={$activeSection === 'pricing'}
+          class:border-transparent={$activeSection !== 'pricing'}
+        >
           Pricing <i class="fa-solid fa-chevron-right text-xl pl-4" />
         </p>
       </button>
@@ -68,15 +75,15 @@
         on:click={() => reroute("#contact")}
         class="border-none outline-none p-2 group duration-200 cursor-pointer text-left"
       >
-        <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">
+        <p
+          class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold border-b-2"
+          class:border-indigo-400={$activeSection === 'contact'}
+          class:border-transparent={$activeSection !== 'contact'}
+        >
           Contact Us <i class="fa-solid fa-chevron-right text-xl pl-4" />
         </p>
       </button>
     </div>
-    <div class="flex flex-col items-center justify-center">
-      <CtAs />
-    </div>
-  </div>
 {/if}
 
 {#if y > outerHeight}
